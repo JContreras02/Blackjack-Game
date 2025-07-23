1e0225c6d1db690abfbe4c6e788a4a52c2ad5b27
JContreras14 <Jonathan.Contreras.JC1@gmail.com>
2025-07-23 19:40:50 -0400

    fixed deal_card func

--- blackjack.py
+++ blackjack.py
@@ -3,26 +3,23 @@ import random
 
 print(blackjack_art.logo)
 
-# Deck of cards (cards will not be removed from the deck)
-deck = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
-
-
-# Function that uses the list to return a random card.
+# Function that returns a random card.
 def deal_card():
-    user_hand = []
-    user_card1 = random.choice(deck)
-    user_card2 = random.choice(deck)
-    user_hand.extend([user_card1, user_card2])
+    """Returns a random card from the deck"""
 
-    print(user_hand)
-    print(sum(user_hand))
+    # Deck of cards (cards will not be removed from the deck)
+    deck = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
+    user_card = random.choice(deck)
+    return user_card
 
-    computer_hand = []
-    computer_card1 = random.choice(deck)
-    computer_card2 = random.choice(deck)
-    computer_hand.extend([computer_card1, computer_card2])
+# Function that sums up the score
+def calculate_score():
+    """Function that takes the list of cards as input and returns the sum as a score."""
+    return
 
-    print(computer_hand)
-    print(sum(computer_hand))
+user_hand = []
+computer_hand = []
 
-deal_card()
+for _ in range(2):
+    user_hand.append(deal_card())
+    computer_hand.append(deal_card())
